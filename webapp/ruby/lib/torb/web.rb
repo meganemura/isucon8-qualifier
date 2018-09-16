d4e4c19185e52b88b66985da55e36956b2d3bd59
mserizawa <sasa.serizawa@gmail.com>
2018-09-16 14:05:04 +0900

    一旦戻し

--- webapp/ruby/lib/torb/web.rb
+++ webapp/ruby/lib/torb/web.rb
@@ -275,11 +275,11 @@ module Torb
       user['total_price'] = db.xquery(price_query, user['id']).first['total_price']
 
       # MEMO: 遅い
-      # rows = db.xquery('SELECT event_id FROM reservations WHERE user_id = ? GROUP BY event_id ORDER BY MAX(IFNULL(canceled_at, reserved_at)) DESC LIMIT 5', user['id'])
-      rows = db.xquery('SELECT event_id, canceled_at, reserved_at FROM reservations WHERE user_id = ?', user['id'])
-      recent_event_ids = rows.sort_by { |row| row['canceled_at'] || row['reserved_at'] }.reverse.map { |row| row['event_id'] }.uniq.slice(0..4)
-      recent_events = recent_event_ids.map do |event_id|
-        event = fetch_event(event_id)
+      rows = db.xquery('SELECT event_id FROM reservations WHERE user_id = ? GROUP BY event_id ORDER BY MAX(IFNULL(canceled_at, reserved_at)) DESC LIMIT 5', user['id'])
+      # rows = db.xquery('SELECT event_id, canceled_at, reserved_at FROM reservations WHERE user_id = ?', user['id'])
+      # recent_event_ids = rows.sort_by { |row| row['canceled_at'] || row['reserved_at'] }.reverse.map { |row| row['event_id'] }.uniq.slice(0..4)
+      recent_events = rows.map do |event_id|
+        event = fetch_event(event_id['event_id'])
         event['sheets'].each { |_, sheet| sheet.delete('detail') }
         event
       end
