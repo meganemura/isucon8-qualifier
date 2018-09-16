26836989bfd453ace34235aeb87588bde9e18012
tknzk <tkm.knzk@gmail.com>
2018-09-16 13:25:02 +0900

    bundle install する場所がちがった

--- pull_restart.sh
+++ pull_restart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/isucon/torb
-git pull && bundle install && sudo systemctl stop torb.ruby && sudo systemctl start torb.ruby
+git pull
+cd /home/isucon/torb/webapp/ruby/
+bundle install && sudo systemctl stop torb.ruby && sudo systemctl start torb.ruby
+sleep(2)
 sudo systemctl status torb.ruby
