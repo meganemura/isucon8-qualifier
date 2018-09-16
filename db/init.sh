732908d0da20423bbf3d946ae2def535909b739b
meganemura <meganemura@users.noreply.github.com>
2018-09-16 11:05:45 +0900

    local-setup

--- db/init.sh
+++ db/init.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+mysql=mysql
+if [ `whoami` != "isucon" ]; then
+  mysql="mysql -h 127.0.0.1"
+fi
+
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 DB_DIR="$ROOT_DIR/db"
 BENCH_DIR="$ROOT_DIR/bench"
 
 export MYSQL_PWD=isucon
 
-mysql -uisucon -e "DROP DATABASE IF EXISTS torb; CREATE DATABASE torb;"
-mysql -uisucon torb < "$DB_DIR/schema.sql"
+$mysql -uisucon -e "DROP DATABASE IF EXISTS torb; CREATE DATABASE torb;"
+$mysql -uisucon torb < "$DB_DIR/schema.sql"
 
 if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
   echo "Run the following command beforehand." 1>&2
@@ -15,6 +20,6 @@ if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
   exit 1
 fi
 
-mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
-gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
-mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+$mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
+gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | $mysql -uisucon torb
+$mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
