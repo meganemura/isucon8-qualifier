a97e43cf128e9daec7b70f0402ea3f45d26bc3ab
tknzk <tkm.knzk@gmail.com>
2018-09-16 12:49:11 +0900

    bundle install, systemctl status

--- pull_restart.sh
+++ pull_restart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/isucon/torb
-git pull && sudo systemctl stop torb.ruby && sudo systemctl start torb.ruby
+git pull && bundle install && sudo systemctl stop torb.ruby && sudo systemctl start torb.ruby
+sudo systemctl status torb.ruby
